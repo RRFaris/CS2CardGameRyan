d7de6933f07db0d333e226ab5eaeb54b5eef52e2
ryanfaris <ryan.faris@menloschool.org>
2024-11-15 13:51:41 -0800

    Built card class.

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
-
 public class Card {
+//  Instance variables
+    private String rank;
+    private String suit;
+    private int value;
+
+//  Constructor
+    public Card(String rank, String suit, int value) {
+        this.rank = rank;
+        this.suit = suit;
+        this.value = value;
+    }
+//  Getter + Setter methods
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+
 }
